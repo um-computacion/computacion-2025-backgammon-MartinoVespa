7c9c7be526be261c02f846c127435ab445a4c525
MartinoVespa <m.vespa@alumno.um.edu.ar>
2025-08-24 23:55:24 -0300

    Inicio del Proyecto

---
+++ core/player.py
@@ -0,0 +1,11 @@
+class Jugador:
+    def __init__(self, nombre, ficha):
+        self.nombre = nombre
+        self.ficha = ficha 
+        self.victorias = 0
+    
+    def incrementar_victorias(self):
+        self.victoria += 1
+
+    def __str__(self):
+        return f"Jugador {self.nombre} ({self.ficha}) - Victorias: {self.victorias}" 
