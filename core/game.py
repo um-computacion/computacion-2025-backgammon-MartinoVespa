bf8a0275facd88628c27d9cf9bc340da9605653a
MartinoVespa <m.vespa@alumno.um.edu.ar>
2025-09-02 23:38:47 -0300

    Implementar la clase game

---
+++ core/game.py
@@ -0,0 +1,9 @@
+def makeMove(self, space, side, steps):
+
+    if side:
+        if self.xJail > 0 and (steps != 0 or space < 18)):
+            return (False, "Asegurate primero de liberar tu ficha de la carcel")
+        elif (self.xJail > 0 and steps == 0 and space > 17):
+            if (self.myBoard[space] > 1):
+                return (False,"Ese lugar ya está ocupado")
+             
