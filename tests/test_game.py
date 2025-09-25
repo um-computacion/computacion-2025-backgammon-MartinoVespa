899702ff05ab763bb0d605dc26c8c00a1db0831a
MartinoVespa <m.vespa@alumno.um.edu.ar>
2025-09-25 20:37:00 -0300

    continuacion del test de la clase game

--- tests/test_game.py
+++ tests/test_game.py
@@ -11,7 +11,6 @@ class TestBackgammonMakeMove(unittest.TestCase):
         self.assertEqual(result, (False, "Asegurate primero de liberar tu ficha de la carcel"))
 
     def test_move_and_capture_sends_to_jail(self):
-
         g = Backgammon()
         g.myBoard[5] = 2 
         g.myBoard[6] = -1  
@@ -26,3 +25,10 @@ class TestBackgammonMakeMove(unittest.TestCase):
         g.myBoard[7] = 0
         res = g.makeMove(space=7, side=False, steps=3)
         self.assertEqual(res, (False, "El espacio esta vacio o el equipo no es el correcto"))    
+
+    def test_wrong_side_or_empty_space(self):
+        g = Backgammon()
+        g.myBoard[7] = 0
+        res = g.makeMove(space=7, side=False, steps=3)
+        self.assertEqual(res, (False, "El espacio esta vacio o el equipo no es el correcto"))
+        
