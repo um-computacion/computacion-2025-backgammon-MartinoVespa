48bcb40c92ab5f0c7a9d5703a5c9364f9974bc82
MartinoVespa <m.vespa@alumno.um.edu.ar>
2025-10-16 23:42:19 -0300

    continuacion de test de la clase board

--- tests/test_board.py
+++ tests/test_board.py
@@ -2,6 +2,16 @@ import unittest
 
 from core import board
 
+class Backgammon:
+    def __init__(self):
+        self.myBoard = [0] * 24
+        self.board = self.myBoard
+        self.xJail = 0
+        self.oJail = 0
+        self.oHome = 15
+        self.oFree = 0
+        self.xHome = 15
+
 def makeMove(self, space, side, steps):
 
         if side:
@@ -90,3 +100,10 @@ def test_populateTop_and_populateBottom():
      assert "|" in top
      assert "|" in bottom
 
+def test_wrong_side_or_empty_space(self):
+        g = Backgammon()
+        g.myBoard[7] = 0
+        res = g.makeMove(space=7, side=False, steps=3)
+        self.assertEqual(res, (False, "El espacio esta vacio o el equipo no es el correcto"))
+
+        
